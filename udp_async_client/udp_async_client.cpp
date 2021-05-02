0a2c888eec3f8147b3722817d0165e44f3f23de3
olinyavod_zh <olinyavod@gmail.com>
2021-05-02 11:35:48 +1000

    Add udp async client

---
+++ udp_async_client/udp_async_client.cpp
@@ -0,0 +1,61 @@
+﻿#include "udp_async_client.h"
+
+using endpoint_iterator = udp::resolver::iterator;
+
+void resolve(asio::io_context& io_context, 
+	const std::string& host,
+	const std::uint16_t port,
+	const std::vector<std::string>& messages)
+{
+	const auto query = std::make_shared<udp::resolver::query>(host, std::to_string(port));
+	const auto resolver = std::make_shared<udp::resolver>(io_context);
+
+	resolver->async_resolve(*query,
+		[&io_context, resolver, &messages](asio::error_code err, endpoint_iterator endpoints)
+		{
+			if(err)
+			{
+				std::cerr << "Error: " << err << ".\n";
+				return 0;
+			}
+			const endpoint_iterator end;
+			auto ep = std::find_if(endpoints, end, [](udp::endpoint ep)
+				{
+					return ep.protocol() == udp::v4();
+				});
+
+			for (const auto& msg : messages)
+				connection::start(io_context, *ep, msg);
+		});
+}
+
+int main(int argc, char* argv[])
+{
+	try 
+	{
+		if(argc < 2)
+		{
+			client::print_usage(argv[0]);
+			return 1;
+		}
+
+		std::string host = "localhost";
+		std::uint16_t port = 9900;
+		std::vector<std::string> messages;
+
+		client::parse_client_args(argc, argv, host, port, messages);
+
+		asio::io_context io_context;
+
+		resolve(io_context, host, port, messages);
+
+		io_context.run();
+		
+		return 0;
+	}
+	catch (std::exception& ex)
+	{
+		std::cerr << "Error: " << ex.what() << ".\n";
+		return 1;
+	}
+}
---
+++ udp_async_client/udp_async_client.h
@@ -0,0 +1,82 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+#include <memory>
+
+#include <asio.hpp>
+
+#include "..\utils.hpp"
+
+using asio::ip::udp;
+
+class connection : public std::enable_shared_from_this<connection>
+{
+private:
+	udp::socket socket_;
+	udp::endpoint endpoint_;
+
+	connection(asio::io_context& io_context,
+		const udp::endpoint& endpoint)
+		:socket_(io_context, udp::endpoint(udp::v4(), 0)),
+		endpoint_(endpoint)
+	{
+
+	}
+
+public:
+	static auto start(asio::io_context& io_context, const udp::endpoint& endpoint, const std::string message)
+	{
+		std::shared_ptr<connection> self(new connection(io_context, endpoint));
+		
+		self->async_send(message);
+		
+		return self;
+	}
+
+private:
+	void async_send(const std::string& message)
+	{
+		const auto self = shared_from_this();
+		auto msg = std::make_shared<std::string>(message);
+
+		socket_.async_send_to(
+			asio::buffer(*msg),
+			endpoint_,
+			[self, msg](asio::error_code err, std::size_t bytes)
+			{
+				if(err)
+				{
+					std::cerr << "Error: " << err << ".\n";
+
+					return;
+				}
+				
+				std::cout << "Sent message: " << *msg << ".\n";
+
+				self->async_read(*msg);
+			});
+	}
+
+	void async_read(const std::string& message)
+	{
+		const auto self = shared_from_this();
+		const auto buffer = std::make_shared<std::array<char, 1024>>();
+		auto msg = std::make_shared<std::string>(message);
+		const auto sender_ep = std::make_shared<udp::endpoint>();
+		socket_.async_receive_from(asio::buffer(*buffer),
+			*sender_ep,
+			[self, buffer, sender_ep, msg](asio::error_code err, std::size_t bytes)
+			{
+				if(err)
+				{
+					std::cerr << "Error: " << err << ".\n";
+					return;
+				}
+				
+				const std::string_view echo(buffer->data(), bytes);
+
+				std::cout << "server "<< *sender_ep << " echoed our " << *msg << ": " << (echo == *msg ? "OK" : "FAIL") << std::endl;			
+			});
+	}	
+};
